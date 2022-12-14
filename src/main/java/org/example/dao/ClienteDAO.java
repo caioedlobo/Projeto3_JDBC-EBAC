f3f5c1de88b895c61a53a364abbb69df91a01a44
Caio Lobo <caioeduardolobo@gmail.com>
2022-12-14 09:57:28 -0300

    create methods buscarTodos and atualizar on ClienteDAO

--- src/main/java/org/example/dao/ClienteDAO.java
+++ src/main/java/org/example/dao/ClienteDAO.java
@@ -3,6 +3,8 @@ package org.example.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.example.dao.jdbc.ConnectionFactory;
 import org.example.domain.Cliente;
@@ -73,6 +75,67 @@ public class ClienteDAO implements IClienteDAO {
             stm = connection.prepareStatement(sql);
             stm.setString(1, cliente.getCodigo());
             return stm.executeUpdate();
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public Integer atualizar(Cliente cliente) throws Exception {
+        Connection connection = null;
+        PreparedStatement stm = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = "update tb_cliente_2 set codigo = ?, nome = ? where id = ?";
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, cliente.getCodigo());
+            stm.setString(2, cliente.getNome());
+            stm.setLong(3, cliente.getId());
+
+            return stm.executeUpdate();
+
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Cliente> buscarTodos() throws Exception {
+        Connection connection = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        List<Cliente> lista = new ArrayList<>();
+        Cliente cliente = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = "select * from tb_cliente_2";
+            stm = connection.prepareStatement(sql);
+            rs = stm.executeQuery();
+
+            while (rs.next()) {
+                cliente = new Cliente();
+                cliente.setId(rs.getLong("id"));
+                cliente.setCodigo(rs.getString("codigo"));
+                cliente.setNome(rs.getString("nome"));
+
+                lista.add(cliente);
+            }
+            return lista;
+
         } catch(Exception e) {
             throw e;
         } finally {
--- src/main/java/org/example/dao/IClienteDAO.java
+++ src/main/java/org/example/dao/IClienteDAO.java
@@ -8,9 +8,8 @@ public interface IClienteDAO {
 
 
     public Integer cadastrar(Cliente cliente) throws Exception;
-
     public Cliente consultar(String codigo) throws Exception;
-
     public Integer excluir(Cliente clienteBD) throws Exception;
-
+    public Integer atualizar(Cliente cliente) throws Exception;
+    public List<Cliente> buscarTodos() throws Exception;
 }
